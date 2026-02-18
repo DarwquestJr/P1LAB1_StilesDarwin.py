2b289d4909f41a5192099509d6d5da1636e21a44
DarwquestJr <darwin.stiles.jr@gmail.com>
2026-02-18 15:56:52 -0500

    Add files via upload

---
+++ P1LAB1_StilesDarwin.py
@@ -0,0 +1,10 @@
+#Darwin Stiles 
+#Feb 18 2026
+#P1LAB1.py
+# python basics with input and print
+
+
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")   
+
+print("Hello, ",first_name, last_name +"!" " Welcome to CTI-110!")    
